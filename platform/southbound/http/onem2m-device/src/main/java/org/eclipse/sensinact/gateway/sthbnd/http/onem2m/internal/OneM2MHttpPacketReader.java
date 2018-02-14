a5ee79aa99e6fb3e3e7fbef2698e1b1560ac2020
Remi Druilhe <remi.druilhe@gmail.com>
2018-02-14 16:46:30 +0100

    First version of the oneM2M bridge
    This bridge enables to connect to oneM2M instances. It has only been tested with Mobius.

---
+++ platform/southbound/http/onem2m-device/src/main/java/org/eclipse/sensinact/gateway/sthbnd/http/onem2m/internal/OneM2MHttpPacketReader.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2018 CEA.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    CEA - initial API and implementation
+ */
+package org.eclipse.sensinact.gateway.sthbnd.http.onem2m.internal;
+
+import org.eclipse.sensinact.gateway.common.bundle.Mediator;
+import org.eclipse.sensinact.gateway.generic.packet.InvalidPacketException;
+import org.eclipse.sensinact.gateway.generic.packet.Packet;
+import org.eclipse.sensinact.gateway.generic.packet.PacketReader;
+import org.eclipse.sensinact.gateway.generic.packet.SimplePacketReader;
+import org.eclipse.sensinact.gateway.sthbnd.http.HttpPacket;
+import org.json.JSONObject;
+
+public class OneM2MHttpPacketReader extends SimplePacketReader<HttpPacket> {
+
+    public static final String DEFAULT_SERVICE_NAME = "container";
+
+	/**
+	 * @param mediator the mediator of the bundle
+	 */
+	public OneM2MHttpPacketReader(Mediator mediator) {
+		super(mediator);
+	}
+
+	/**
+	 * @inheritDoc
+	 *
+	 * @see PacketReader#parse(Packet)
+	 */
+	@Override
+	public void parse(HttpPacket packet) throws InvalidPacketException {
+		try {
+			JSONObject content = new JSONObject(new String(packet.getBytes()));
+
+			if(mediator.isDebugLoggable()) {
+			    mediator.debug(content.toString());
+            }
+
+			if(content.has("m2m:uril")) {
+                String[] uris = content.getString("m2m:uril").split(" ");
+
+                for(String uri : uris) {
+                    String[] elements = uri.split("/");
+
+                    if(elements.length >= 3) {
+                        if(elements.length >= 5 && elements.length < 6) {
+                            super.setResourceId(elements[4]);
+                            super.setServiceId(elements[3]);
+                        } else if("admin".equalsIgnoreCase(elements[3])) {
+                            super.setServiceId(elements[3]);
+                        } else {
+                            super.setResourceId(elements[3]);
+                            super.setServiceId(DEFAULT_SERVICE_NAME);
+                        }
+
+                        super.setServiceProviderId(elements[2]);
+                        super.configure();
+                    }
+                }
+            }
+		} catch(Exception e) {
+			mediator.error(e);
+			throw new InvalidPacketException(e);
+		}
+	}
+}
---
+++ platform/southbound/http/onem2m-device/src/main/java/org/eclipse/sensinact/gateway/sthbnd/http/onem2m/osgi/Activator.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2018 CEA.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    CEA - initial API and implementation
+ */
+package org.eclipse.sensinact.gateway.sthbnd.http.onem2m.osgi;
+
+import org.eclipse.sensinact.gateway.common.bundle.AbstractActivator;
+import org.eclipse.sensinact.gateway.core.SensiNactResourceModelConfiguration.BuildPolicy;
+import org.eclipse.sensinact.gateway.generic.ExtModelConfiguration;
+import org.eclipse.sensinact.gateway.generic.ExtModelInstanceBuilder;
+import org.eclipse.sensinact.gateway.generic.InvalidProtocolStackException;
+import org.eclipse.sensinact.gateway.generic.Task;
+import org.eclipse.sensinact.gateway.sthbnd.http.annotation.HttpTaskConfiguration;
+import org.eclipse.sensinact.gateway.sthbnd.http.annotation.HttpTasks;
+import org.eclipse.sensinact.gateway.sthbnd.http.annotation.KeyValuePair;
+import org.eclipse.sensinact.gateway.sthbnd.http.annotation.SimpleHttpTask;
+import org.eclipse.sensinact.gateway.sthbnd.http.onem2m.internal.OneM2MHttpGetConfigurator;
+import org.eclipse.sensinact.gateway.sthbnd.http.onem2m.task.OneM2MDiscoveryTask;
+import org.eclipse.sensinact.gateway.sthbnd.http.onem2m.task.OneM2MGetTask;
+import org.eclipse.sensinact.gateway.sthbnd.http.smpl.*;
+
+@HttpTasks(tasks = {
+    @SimpleHttpTask(
+        commands = { Task.CommandType.GET },
+        configuration = @HttpTaskConfiguration(
+            host = "$(http.onem2m.host)",
+            port = "$(http.onem2m.port)",
+            path = "/$(http.onem2m.cse.base)",
+            acceptType = "application/json",
+            contentType = "application/json",
+            headers = {
+                @KeyValuePair(key = "X-M2M-Origin", value = "SOrigin")
+            },
+            content = OneM2MHttpGetConfigurator.class,
+            direct = true
+        )
+    )
+})
+public class Activator extends HttpActivator {
+
+    /**
+     * @inheritDoc
+     *
+     * @see AbstractActivator#doStart()
+     */
+    @Override
+    public void doStart() throws Exception {
+        super.mediator.setTaskProcessingContextHandler(this.getProcessingContextHandler());
+        this.mediator.setTaskProcessingContextFactory(this.getTaskProcessingContextFactory());
+        this.mediator.setChainedTaskProcessingContextFactory(this.getChainedTaskProcessingContextFactory());
+
+        ExtModelConfiguration configuration = new ExtModelInstanceBuilder(mediator, getPacketType())
+                .withStartAtInitializationTime(isStartingAtInitializationTime())
+                .withServiceBuildPolicy(BuildPolicy.BUILD_NON_DESCRIBED.getPolicy())
+                .withResourceBuildPolicy(BuildPolicy.BUILD_NON_DESCRIBED.getPolicy())
+                .withDesynchronization(true)
+                .buildConfiguration(getResourceDescriptionFile(), getDefaults());
+
+        this.endpoint = this.configureProtocolStackEndpoint();
+        this.connect(configuration);
+    }
+
+    /**
+     * @inheritDoc
+     *
+     * @see HttpActivator#connect(ExtModelConfiguration)
+     */
+    @Override
+	public void connect(ExtModelConfiguration configuration) throws InvalidProtocolStackException {
+        super.endpoint.registerDiscoveryTask(new OneM2MDiscoveryTask(mediator, endpoint));
+
+        super.endpoint.setGetTaskType(OneM2MGetTask.class);
+
+        super.connect(configuration);
+    }
+}
