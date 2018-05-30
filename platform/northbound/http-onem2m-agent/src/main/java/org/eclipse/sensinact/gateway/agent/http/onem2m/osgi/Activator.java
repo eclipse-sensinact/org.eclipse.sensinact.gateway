b102f690e74eb78bd9dce3ee61f92fdb0eeb46c9
Jander Nascimento <jbotnascimento@gmail.com>
2018-05-30 09:30:34 +0200

    Initial onem2m bridge version

---
+++ platform/northbound/http-onem2m-agent/src/main/java/org/eclipse/sensinact/gateway/agent/http/onem2m/osgi/Activator.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2017 CEA.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    CEA - initial API and implementation
+ */
+
+package org.eclipse.sensinact.gateway.agent.http.onem2m.osgi;
+
+import java.awt.Window.Type;
+
+import org.eclipse.sensinact.gateway.agent.http.onem2m.internal.SnaEventOneM2MHttpHandler;
+import org.eclipse.sensinact.gateway.common.annotation.Property;
+import org.eclipse.sensinact.gateway.common.bundle.AbstractActivator;
+import org.eclipse.sensinact.gateway.common.bundle.Mediator;
+import org.eclipse.sensinact.gateway.common.execution.Executable;
+import org.eclipse.sensinact.gateway.core.Core;
+import org.eclipse.sensinact.gateway.core.message.SnaFilter;
+import org.eclipse.sensinact.gateway.core.message.SnaMessage;
+import org.osgi.framework.BundleContext;
+
+/**
+ * Extended {@link AbstractActivator}
+ */
+public class Activator extends AbstractActivator<Mediator> {
+
+	
+    @Property(name = "org.eclipse.sensinact.gateway.northbound.http.onem2m.csebase")
+    public String cseBase;
+    
+    private SnaEventOneM2MHttpHandler handler;
+
+	private String registration;
+
+    /** @inheritDoc
+     *
+     * @see AbstractActivator#doStart()
+     */
+    @Override
+    public void doStart() throws Exception {
+    	
+    	this.handler = new SnaEventOneM2MHttpHandler(cseBase);
+    	this.registration = super.mediator.callService(Core.class, new Executable<Core,String>()
+		{
+			@Override
+			public String execute(Core core) throws Exception {
+
+				SnaFilter filter = new SnaFilter(mediator,"(\\/slider\\/cursor\\/position(\\/[^\\/]+)*)|(\\/(slider|(lora\\-tracker\\-[0-9]+))\\/admin\\/location(\\/[^\\/]+)*)",true,false);
+				filter.addHandledType(SnaMessage.Type.values());
+				return core.registerAgent(mediator, handler, filter);
+			}
+		});
+    }
+
+	@Override
+	public void doStop() throws Exception
+	{
+		super.mediator.callService(Core.class, new Executable<Core,Void>()
+		{
+			@Override
+			public Void execute(Core core) throws Exception {
+	
+				 core.unregisterAgent(registration);
+				 return null;
+			}
+		});
+	}
+
+	@Override
+	public Mediator doInstantiate(BundleContext context) {
+		
+		return new Mediator(context);
+	}
+}
