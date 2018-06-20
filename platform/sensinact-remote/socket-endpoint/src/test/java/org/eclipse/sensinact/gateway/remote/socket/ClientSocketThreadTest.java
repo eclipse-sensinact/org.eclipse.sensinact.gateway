976d69c0c3ea2406a8ead48d0843d95046fbce15
Stéphane Bergeon <Stephane.BERGEON@cea.fr>
2018-06-20 19:10:57 +0200

    improved management of the ClientSocketThread requests map to avoid memory leak + added test
    Signed-off-by: Stéphane Bergeon <Stephane.BERGEON@cea.fr>

---
+++ platform/sensinact-remote/socket-endpoint/src/test/java/org/eclipse/sensinact/gateway/remote/socket/ClientSocketThreadTest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2017 CEA.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    CEA - initial API and implementation
+ */
+package org.eclipse.sensinact.gateway.remote.socket;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author <a href="mailto:stephane.bergeon@cea.fr">Stéphane Bergeon</a>
+ */
+public class ClientSocketThreadTest {
+    
+    public ClientSocketThreadTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testUUIDtoTimeStamp() {
+        final long timestamp = System.currentTimeMillis();
+        try {
+            ClientSocketThread instance = new ClientSocketThread(null, null, "localhost", 80);
+            final String uuid = instance.generateUUID(timestamp);
+            System.out.print("at " + timestamp);
+            System.out.println(" generated uuid= " + uuid);
+            long requestTime = instance.getRequestTime(uuid);
+            System.out.println("recovered time stamp for request= " + requestTime);
+            assertEquals("error in uuid generation / time stamp recovery", timestamp, requestTime);
+        } catch (IOException ex) {
+            Logger.getLogger(ClientSocketThreadTest.class.getName()).log(Level.SEVERE, null, ex);
+            fail("unexpected error in testUUIDtoTimeStamp");
+        }
+    }
+}
