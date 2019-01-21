aa58f8d854f1b16c0d4ca6470322d5c90552377e
Jander Nascimento <jbotnascimento@gmail.com>
2019-01-21 15:35:03 +0100

    Add username password support for mqtt bridge

--- platform/northbound/mqtt/mqtt-agent/src/main/java/org/eclipse/sensinact/gateway/agent/mqtt/inst/osgi/Activator.java
+++ platform/northbound/mqtt/mqtt-agent/src/main/java/org/eclipse/sensinact/gateway/agent/mqtt/inst/osgi/Activator.java
@@ -27,17 +27,26 @@ public class Activator extends AbstractMqttActivator {
     public String qos;
     @Property(defaultValue = "/",mandatory = false)
     public String prefix;
-
+    @Property(defaultValue = "tcp",mandatory = false)
+    public String protocol;
+    @Property(mandatory = false)
+    String username;
+    @Property(mandatory = false)
+    String password;
     /**
      * @inheritDoc
      * @see AbstractActivator#doStart()
      */
     @Override
     public void doStart() throws Exception {
-        final String broker = String.format("tcp://%s:%s",host,port);
+        final String broker = String.format("%s://%s:%s",protocol,host,port);
         mediator.setProperty("broker",broker);
         mediator.setProperty("qos",qos);
         mediator.setProperty("prefix",prefix);
+        if(username!=null){
+            mediator.setProperty("username",username);
+            mediator.setProperty("password",password);
+        }
         doStart(new SnaEventEventHandler(broker,new Integer(qos),prefix));
     }
 }
