48d56bcc65e1ab0fe1b4ef4abd9c8cdc74db49b6
Jander Nascimento <jbotnascimento@gmail.com>
2019-04-29 11:37:51 +0200

    Remove dependency from mqtt-server embedded to the sensinact core
    This dependency was remove in order to simplify its use in the integration test

--- platform/tools/mqtt-server/src/main/java/org/sensinact/mqtt/server/osgi/Activator.java
+++ platform/tools/mqtt-server/src/main/java/org/sensinact/mqtt/server/osgi/Activator.java
@@ -10,26 +10,39 @@
  */
 package org.sensinact.mqtt.server.osgi;
 
-import org.eclipse.sensinact.gateway.common.bundle.Mediator;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
 import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.cm.*;
 import org.sensinact.mqtt.server.MQTTException;
 import org.sensinact.mqtt.server.MQTTServerService;
 import org.sensinact.mqtt.server.impl.MQTTServerImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.util.Dictionary;
 import java.util.Hashtable;
 
-public class Activator implements BundleActivator {//extends AbstractActivator
+public class Activator implements BundleActivator,SynchronousConfigurationListener {
     private Boolean autoStart=false;
     private Integer port=1883;
     private Logger LOG = LoggerFactory.getLogger(Activator.class);
     private MQTTServerService service;
     private ServiceRegistration sr;
+    private BundleContext bundleContext;
 
-    public void start(BundleContext bundleContext) throws Exception {
+    public void start(final BundleContext bundleContext) throws Exception {
+        this.bundleContext=bundleContext;
+
+        //ServiceReference configadminsr=bundleContext.getServiceReferences(ConfigurationAdmin.class.getCanonicalName(),null)[0];
+
+        bundleContext.registerService(ConfigurationListener.class.getCanonicalName(),this,new Hashtable<String,String>());
+
+    }
+
+    protected void startService(BundleContext bundleContext){
         service = new MQTTServerImpl(bundleContext);
         if (autoStart) {
             LOG.debug("Start MQTT Service autoStart enabled.");
@@ -48,5 +61,32 @@ public class Activator implements BundleActivator {//extends AbstractActivator
         sr.unregister();
         service.stopServer();
     }
-    
+
+    @Override
+    public void configurationEvent(ConfigurationEvent configurationEvent) {
+
+        if(configurationEvent.getPid().equals("mqtt.server")) {
+
+            try {
+
+                ConfigurationAdmin configurationAdmin=bundleContext.getService(configurationEvent.getReference());
+                Configuration confSensinact= configurationAdmin.getConfiguration("mqtt.server");;
+                final Dictionary<String, Object> prop=confSensinact.getProperties();
+
+                final Object autoStartString=confSensinact.getProperties().get("autoStart");
+                final Object portString=confSensinact.getProperties().get("port");
+
+                autoStart=Boolean.parseBoolean(autoStartString!=null?autoStartString.toString():"false");
+                port=Integer.parseInt(portString!=null?portString.toString():"1883");
+
+                if(prop!=null&&prop.get("port")!=null&&prop.get("autoStart")!=null){
+                    startService(bundleContext);
+                }
+            } catch (Exception e) {
+
+                e.printStackTrace();
+            }
+
+        }
+    }
 }
