d1d85547cc0e712fd8c80dfe72ff18229bcbf50d
Christophe Munilla <cmunilla@cmssi.fr>
2020-09-21 00:05:23 +0200

    Introduce archetypes
    introduce basic archetype
    
    introduce http archetype

---
+++ distribution/archetypes/sensinact-basis-archetype/src/main/resources/archetype-resources/src/main/java/osgi/Activator.java
@@ -0,0 +1,37 @@
+/**
+ * #%L
+ * sensiNact IoT Gateway - Core
+ * %%
+ * Copyright (C) 2015 CEA
+ * %%
+ * sensiNact - 2015
+ * 
+ * CEA - Commissariat a l'energie atomique et aux energies alternatives
+ * 17 rue des Martyrs
+ * 38054 Grenoble
+ * France
+ * 
+ * Copyright(c) CEA
+ * All Rights Reserved
+ * #L%
+ */
+package ${package}.osgi;
+
+import java.io.FileOutputStream;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+import java.util.ConcurrentModificationException;
+import java.util.List;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.BundleException;
+
+import org.eclipse.sensinact.gateway.common.bundle.Mediator;
+import org.eclipse.sensinact.gateway.generic.BasisActivator;
+
+/**
+ * Bundle Activator
+ */
+public class Activator extends BasisActivator
+{		
+}
---
+++ distribution/archetypes/sensinact-http-archetype/src/main/resources/archetype-resources/src/main/java/osgi/Activator.java
@@ -0,0 +1,32 @@
+/**
+ * #%L
+ * sensiNact IoT Gateway - southebound bridge
+ * %%
+ * Copyright (C) 2015 CEA
+ * %%
+ * sensiNact - 2015
+ * 
+ * CEA - Commissariat a l'energie atomique et aux energies alternatives
+ * 17 rue des Martyrs
+ * 38054 Grenoble
+ * France
+ * 
+ * Copyright(c) CEA
+ * All Rights Reserved
+ * #L%
+ */
+package ${package}.osgi;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.BundleException;
+
+import fr.cea.sna.gateway.sthbnd.http.smpl.HttpActivator;
+
+
+/**
+ * Bundle Activator
+ */
+public class Activator extends HttpActivator
+{	
+
+}
