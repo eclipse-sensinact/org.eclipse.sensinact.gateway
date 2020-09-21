c64003e1607f547c63c29e270b7a7dcb46c3ac73
Christophe Munilla <cmunilla@cmssi.fr>
2020-09-21 14:09:01 +0200

    Remove useless imports
    Remove invalid headers

--- distribution/archetypes/sensinact-http-archetype/src/main/resources/archetype-resources/src/main/java/osgi/Activator.java
+++ distribution/archetypes/sensinact-http-archetype/src/main/resources/archetype-resources/src/main/java/osgi/Activator.java
@@ -1,30 +1,10 @@
-/**
- * #%L
- * sensiNact IoT Gateway - southebound bridge
- * %%
- * Copyright (C) 2015 CEA
- * %%
- * sensiNact - 2015
- * 
- * CEA - Commissariat a l'energie atomique et aux energies alternatives
- * 17 rue des Martyrs
- * 38054 Grenoble
- * France
- * 
- * Copyright(c) CEA
- * All Rights Reserved
- * #L%
- */
 package ${package}.osgi;
-
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.BundleException;
 
 import fr.cea.sna.gateway.sthbnd.http.smpl.HttpActivator;
 
 
 /**
- * Bundle Activator
+ * Handle activation/deactivation of the module
  */
 public class Activator extends HttpActivator
 {	
