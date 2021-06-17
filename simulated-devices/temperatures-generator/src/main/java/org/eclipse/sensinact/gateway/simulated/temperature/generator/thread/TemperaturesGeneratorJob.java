6132d42fbd73b6faabedb6683d4b5f4c524777de
Tim Ward <timothyjward@apache.org>
2021-06-17 10:18:59 +0100

    Improve Temperature Generator scaling
    The simulated temperature sensors use one thread per service provider. This causes sensiNact to have very high resource usage for large numbers of sensors. This fix changes the temperature generator to use a small threadpool and scheduling, rather than separate threads and sleeps, reducing overhead considerably.
    
    Signed-off-by: Tim Ward <timothyjward@apache.org>

--- simulated-devices/temperatures-generator/src/main/java/org/eclipse/sensinact/gateway/simulated/temperature/generator/thread/TemperaturesGeneratorJob.java
+++ simulated-devices/temperatures-generator/src/main/java/org/eclipse/sensinact/gateway/simulated/temperature/generator/thread/TemperaturesGeneratorJob.java
@@ -10,19 +10,26 @@
  */
 package org.eclipse.sensinact.gateway.simulated.temperature.generator.thread;
 
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 import org.eclipse.sensinact.gateway.generic.local.LocalProtocolStackEndpoint;
 import org.eclipse.sensinact.gateway.generic.packet.InvalidPacketException;
 import org.eclipse.sensinact.gateway.simulated.temperature.generator.discovery.TemperaturesGeneratorDiscoveryPacket;
 import org.eclipse.sensinact.gateway.simulated.temperature.generator.parser.DeviceInfo;
 import org.eclipse.sensinact.gateway.simulated.temperature.generator.reader.TemperaturesGeneratorPacket;
 
-public class TemperaturesGeneratorThread extends Thread implements Runnable {
-    private LocalProtocolStackEndpoint<TemperaturesGeneratorPacket> connector;
-    private DeviceInfo deviceInfo;
+public class TemperaturesGeneratorJob implements Runnable {
+    private final LocalProtocolStackEndpoint<TemperaturesGeneratorPacket> connector;
+    private final DeviceInfo deviceInfo;
+	private final ScheduledExecutorService worker;
+	
+	volatile int index = 1;
 
-    public TemperaturesGeneratorThread(LocalProtocolStackEndpoint<TemperaturesGeneratorPacket> connector, DeviceInfo deviceInfo) {
+    public TemperaturesGeneratorJob(LocalProtocolStackEndpoint<TemperaturesGeneratorPacket> connector, DeviceInfo deviceInfo, ScheduledExecutorService worker) {
         this.connector = connector;
         this.deviceInfo = deviceInfo;
+		this.worker = worker;
         try {
             this.connector.process(new TemperaturesGeneratorDiscoveryPacket(deviceInfo.getServiceProviderId(), deviceInfo.getLocation(), deviceInfo.getTemperatures()[0]));
         } catch (InvalidPacketException e) {
@@ -32,20 +39,13 @@ public class TemperaturesGeneratorThread extends Thread implements Runnable {
 
     @Override
     public void run() {
-        int index = 1;
-        while (true) {
-            try {
-                this.connector.process(new TemperaturesGeneratorPacket(deviceInfo.getServiceProviderId(), deviceInfo.getTemperatures()[index]));
-                index = (index + 1) % 12;
-            } catch (InvalidPacketException e) {
-                e.printStackTrace();
-            }
-            try {
-                Thread.sleep(deviceInfo.getSleepTime());
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-                break;
-            }
+        int index = this.index;
+        try {
+        	this.connector.process(new TemperaturesGeneratorPacket(deviceInfo.getServiceProviderId(), deviceInfo.getTemperatures()[index]));
+        	this.index = (index + 1) % 12;
+        } catch (InvalidPacketException e) {
+        	e.printStackTrace();
         }
+        worker.schedule(this, deviceInfo.getSleepTime(), TimeUnit.MILLISECONDS);
     }
 }
--- simulated-devices/temperatures-generator/src/main/java/org/eclipse/sensinact/gateway/simulated/temperature/generator/thread/TemperaturesGeneratorThreadManager.java
+++ simulated-devices/temperatures-generator/src/main/java/org/eclipse/sensinact/gateway/simulated/temperature/generator/thread/TemperaturesGeneratorThreadManager.java
@@ -10,33 +10,56 @@
  */
 package org.eclipse.sensinact.gateway.simulated.temperature.generator.thread;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import org.eclipse.sensinact.gateway.generic.local.LocalProtocolStackEndpoint;
 import org.eclipse.sensinact.gateway.simulated.temperature.generator.parser.DeviceInfo;
 import org.eclipse.sensinact.gateway.simulated.temperature.generator.reader.TemperaturesGeneratorPacket;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class TemperaturesGeneratorThreadManager {
-    private Set<TemperaturesGeneratorThread> threadList;
+    private final ScheduledExecutorService worker;
+    
+    Map<String, TemperaturesGeneratorJob> jobs = new HashMap<>();
 
     public TemperaturesGeneratorThreadManager(LocalProtocolStackEndpoint<TemperaturesGeneratorPacket> connector, Set<DeviceInfo> deviceInfos) {
-        this.threadList = new HashSet<TemperaturesGeneratorThread>();
+        // Make a fixed pool of three threads
+        AtomicInteger counter = new AtomicInteger();
+        ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(1, r -> {
+            Thread t = new Thread(r, "Temperature Generation Thread " + counter.incrementAndGet());
+            return t;
+        });
+        executor.allowCoreThreadTimeOut(false);
+        executor.setCorePoolSize(3);
+        executor.setMaximumPoolSize(3);
+        
+        worker = executor;
+        
         for (DeviceInfo deviceInfo : deviceInfos) {
-            TemperaturesGeneratorThread thread = new TemperaturesGeneratorThread(connector, deviceInfo);
-            this.threadList.add(thread);
+            TemperaturesGeneratorJob job = new TemperaturesGeneratorJob(connector, deviceInfo, worker);
+            this.jobs.put(deviceInfo.getServiceProviderId(), job);
         }
     }
 
     public void startThreads() {
-        for (TemperaturesGeneratorThread thread : threadList) {
-            thread.start();
+        
+        for (TemperaturesGeneratorJob job : jobs.values()) {
+            worker.execute(job);
         }
     }
 
     public void stopThreads() {
-        for (TemperaturesGeneratorThread thread : threadList) {
-            thread.interrupt();
+        worker.shutdownNow();
+        try {
+            worker.awaitTermination(10, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
         }
     }
 }
