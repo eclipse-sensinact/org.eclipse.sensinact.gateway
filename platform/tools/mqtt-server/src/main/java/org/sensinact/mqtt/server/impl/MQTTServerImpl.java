bbe04afd757f83561b25c889335e12509a7b2505
Stefan Bischof <stbischof@bipolis.org>
2021-10-29 13:46:24 +0200

    sna-mqtt-server - Activator -> Component
    Change-Id: I55c77f9ef2ddcbac6e64d3d34ea97b634dafc181
    Signed-off-by: Stefan Bischof <stbischof@bipolis.org>

--- platform/tools/mqtt-server/src/main/java/org/sensinact/mqtt/server/impl/MQTTServerImpl.java
+++ platform/tools/mqtt-server/src/main/java/org/sensinact/mqtt/server/impl/MQTTServerImpl.java
@@ -10,30 +10,81 @@
  */
 package org.sensinact.mqtt.server.impl;
 
-import io.moquette.BrokerConstants;
-import io.moquette.interception.InterceptHandler;
-import io.moquette.server.config.IConfig;
-import io.moquette.server.config.MemoryConfig;
-import org.osgi.framework.BundleContext;
-import org.sensinact.mqtt.server.MQTTException;
-import org.sensinact.mqtt.server.MQTTServerService;
-import org.sensinact.mqtt.server.internal.SensiNactServer;
-
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Properties;
 
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.metatype.annotations.Designate;
+import org.osgi.service.metatype.annotations.ObjectClassDefinition;
+import org.sensinact.mqtt.server.MQTTException;
+import org.sensinact.mqtt.server.MQTTServerService;
+import org.sensinact.mqtt.server.internal.SensiNactServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.moquette.BrokerConstants;
+import io.moquette.interception.InterceptHandler;
+import io.moquette.server.config.IConfig;
+import io.moquette.server.config.MemoryConfig;
+
 /**
  * MQTT Server wrapper implementation, to hide dependencies and provide a MQTT server as a auto sufficient bundle
  */
+@Designate(ocd = MQTTServerImpl.Config.class)
+@Component(immediate = true, service = MQTTServerService.class,configurationPid = MQTTServerImpl.CONFIGURATION_PID)
 public class MQTTServerImpl implements MQTTServerService {
-    private final BundleContext bundleContext;
+    private Logger LOG = LoggerFactory.getLogger(MQTTServerImpl.class);
+
+    @ObjectClassDefinition()
+	@interface Config{
+		
+		boolean autostart() default false;
+		String host() default "";
+		int port() default 0;
+	}
+	
+   public final static String CONFIGURATION_PID="mqtt.server";
+
     SensiNactServer server;
     private HashMap<String, SensiNactServer> mqttServiceMap = new HashMap<>();
-
-    public MQTTServerImpl(BundleContext bundleContext) {
-        this.bundleContext = bundleContext;
+    
+    @Activate
+    private BundleContext bc;
+    
+    
+    @Activate
+    public void updateConfig(Config config) {
+    	
+    	if (config.autostart()) {
+    		try {
+    			if (config.port() > 0) {
+    				startService(config.host(), String.valueOf(config.port()));
+    			} else {
+    				startService();
+    			}
+    		} catch (MQTTException e) {
+    			LOG.error("Failed to start MQTT service", e);
+    		}
+    	}
+    	
     }
+    
+    @Deactivate
+    public void stopServer() {
+    	for (SensiNactServer server : mqttServiceMap.values()) {
+    		try {
+    			server.stopServer();
+    		} catch (Exception e) {
+    		} finally {
+    			mqttServiceMap = new HashMap<>();
+    		}
+    	}
+    }
+
 
     private void pushService(String id, SensiNactServer server) throws MQTTException {
         if (mqttServiceMap.get(id) != null) {
@@ -61,7 +112,7 @@ public class MQTTServerImpl implements MQTTServerService {
             properties.put(BrokerConstants.HOST_PROPERTY_NAME, host);
             properties.put(BrokerConstants.PORT_PROPERTY_NAME, port);
             properties.put(BrokerConstants.WEB_SOCKET_PORT_PROPERTY_NAME, "disabled");
-            server = new SensiNactServer(bundleContext);
+            server = new SensiNactServer(bc);
             final String id = String.format("%s:%s", host, port);
             if (mqttServiceMap.get(id) != null) {
                 throw new MQTTException(String.format("MQTT service id %s already exists", id));
@@ -105,17 +156,6 @@ public class MQTTServerImpl implements MQTTServerService {
         return mqttServiceMap.get(id) != null;
     }
 
-    /**
-     * @inheritDoc
-     */
-    public void stopServer() {
-        for (SensiNactServer server : mqttServiceMap.values()) {
-            try {
-                server.stopServer();
-            } catch (Exception e) {
-            } finally {
-                mqttServiceMap = new HashMap<>();
-            }
-        }
-    }
+
 }
+	
