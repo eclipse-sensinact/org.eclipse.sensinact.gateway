c10422d14b31ce1f0f1de35f27ca84b87a31f4d2
Stefan Bischof <stbischof@bipolis.org>
2022-10-18 16:15:00 +0200

    add license-checker and -fixes
    Signed-off-by: Stefan Bischof <stbischof@bipolis.org>

--- prototype/examples/notification/src/main/java/notification/_02_ClientNotification.java
+++ prototype/examples/notification/src/main/java/notification/_02_ClientNotification.java
@@ -1,11 +1,14 @@
 /*********************************************************************
-* Copyright (c) 2022 Kentyou and others
+* Copyright (c) 2022 Contributors to the Eclipse Foundation.
 *
 * This program and the accompanying materials are made
 * available under the terms of the Eclipse Public License 2.0
 * which is available at https://www.eclipse.org/legal/epl-2.0/
 *
 * SPDX-License-Identifier: EPL-2.0
+*
+* Contributors:
+*   Kentyou - initial implementation 
 **********************************************************************/
 package notification;
 
