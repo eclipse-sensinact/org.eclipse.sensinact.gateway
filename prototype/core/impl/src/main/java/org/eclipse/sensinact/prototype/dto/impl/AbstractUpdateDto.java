a52655e7a98c3d07e4a8366f63795a32cefff37a
Tim Ward <timothyjward@apache.org>
2022-11-03 12:32:23 +0000

    Use super-linter for editorconfig validation (#40)
    * Use super-linter for editorconfig validation
    * Add JavaDoc to test the EditorConfig check
    * Only lint the prototype folder
    
    Signed-off-by: Tim Ward <timothyjward@apache.org>

--- prototype/core/impl/src/main/java/org/eclipse/sensinact/prototype/dto/impl/AbstractUpdateDto.java
+++ prototype/core/impl/src/main/java/org/eclipse/sensinact/prototype/dto/impl/AbstractUpdateDto.java
@@ -8,7 +8,7 @@
 * SPDX-License-Identifier: EPL-2.0
 *
 * Contributors:
-*   Kentyou - initial implementation 
+*   Kentyou - initial implementation
 **********************************************************************/
 package org.eclipse.sensinact.prototype.dto.impl;
 
@@ -16,13 +16,28 @@ import java.time.Instant;
 
 public abstract class AbstractUpdateDto {
 
+    /**
+     * The model to use, if null then a unique model may be created
+     */
     public String model;
 
+    /**
+     * The provider name for this update. Must be set
+     */
     public String provider;
 
+    /**
+     * The service name for this update. Must be set
+     */
     public String service;
 
+    /**
+     * The resource name for this update. Must be set
+     */
     public String resource;
 
+    /**
+     * The timestamp for this update. If not set then the current time is used.
+     */
     public Instant timestamp;
 }
