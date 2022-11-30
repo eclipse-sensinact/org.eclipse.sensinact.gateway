b2b7a85aa6b3d15d4ac3db766c0a2d49e71ba94b
Tim Ward <timothyjward@apache.org>
2022-11-30 11:18:05 +0000

    Add a Jakarta REST whitboard feature based on Servlet Whiteboard
    Signed-off-by: Tim Ward <timothyjward@apache.org>

--- prototype/distribution/features/jakarta-rest-whiteboard-feature/src/test/java/org/eclipse/sensinact/gateway/feature/integration/jakartarest/RestWhiteboardIntegrationTest.java
+++ prototype/distribution/features/jakarta-rest-whiteboard-feature/src/test/java/org/eclipse/sensinact/gateway/feature/integration/jakartarest/RestWhiteboardIntegrationTest.java
@@ -10,10 +10,11 @@
 * Contributors:
 *   Kentyou - initial implementation
 **********************************************************************/
-package org.eclipse.sensinact.gateway.feature.integration.jakartaservlet;
+package org.eclipse.sensinact.gateway.feature.integration.jakartarest;
 
 import static java.lang.ProcessBuilder.Redirect.PIPE;
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
@@ -21,20 +22,27 @@ import static org.junit.jupiter.api.Assertions.fail;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ConnectException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpConnectTimeoutException;
 import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublishers;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.Duration;
 
+import org.eclipse.sensinact.gateway.feature.integration.jakartarest.resource.Resource;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.ops4j.pax.tinybundles.core.TinyBundles;
+import org.osgi.framework.Constants;
 
-class ServletWhiteboardIntegrationTest {
+class RestWhiteboardIntegrationTest {
 
     private static Process OSGI_PROCESS;
 
@@ -43,6 +51,20 @@ class ServletWhiteboardIntegrationTest {
 
         OSGI_PROCESS = new ProcessBuilder("java", "-Dsensinact.config.dir=src/it/resources/config", "-jar",
                 "target/it/launcher.jar").redirectInput(PIPE).redirectOutput(PIPE).redirectErrorStream(true).start();
+
+        try (InputStream is = TinyBundles.bundle()
+                .symbolicName("org.eclipse.sensinact.gateway.feature.integration.jakartarest").add(Resource.class)
+                .set(Constants.BUNDLE_VERSION, "0.0.1").set("-noee", "true").build(TinyBundles.withBnd())) {
+            Files.copy(is, Paths.get("target/it/test.jar"), REPLACE_EXISTING);
+
+            OutputStream stream = OSGI_PROCESS.getOutputStream();
+            stream.write(("\nb = install file:target/it/test.jar\n").getBytes(UTF_8));
+            stream.flush();
+
+            // Slightly odd, but the previous output is "Bundle ID: <id>"
+            stream.write("\nstart ($b substring 11)\n".getBytes(UTF_8));
+            stream.flush();
+        }
     }
 
     @AfterAll
@@ -73,18 +95,28 @@ class ServletWhiteboardIntegrationTest {
     void testHttp() throws Exception {
         HttpClient client = HttpClient.newBuilder().connectTimeout(Duration.ofSeconds(5)).build();
 
-        HttpRequest request = HttpRequest.newBuilder(URI.create("http://localhost:8081")).GET().build();
+        HttpRequest request = HttpRequest.newBuilder(URI.create("http://localhost:8082/test")).GET().build();
 
         // Try a few times to check an Http endpoint is there
         for (int i = 0; i < 10; i++) {
             try {
                 HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-                assertEquals(404, response.statusCode());
-                return;
+                if (200 == response.statusCode()) {
+                    assertEquals("Hello World", response.body());
+
+                    request = HttpRequest.newBuilder(URI.create("http://localhost:8082/test"))
+                            .POST(BodyPublishers.ofString("foobar")).build();
+                    response = client.send(request, BodyHandlers.ofString());
+
+                    assertEquals(200, response.statusCode());
+                    assertEquals("Hello foobar", response.body());
+
+                    return;
+                }
             } catch (ConnectException | HttpConnectTimeoutException timeout) {
                 // Just try again in a little while
-                Thread.sleep(1000);
             }
+            Thread.sleep(1000);
         }
         fail("Did not contact the server");
     }
