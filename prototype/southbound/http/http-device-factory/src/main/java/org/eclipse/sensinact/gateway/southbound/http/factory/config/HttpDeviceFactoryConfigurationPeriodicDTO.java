c38aca8b04fd837a0a37734a34dc137ab7c740cf
Thomas Calmant <thomas.calmant@gmail.com>
2023-01-10 16:46:28 +0100

    HTTP device factory implementation

---
+++ prototype/southbound/http/http-device-factory/src/main/java/org/eclipse/sensinact/gateway/southbound/http/factory/config/HttpDeviceFactoryConfigurationPeriodicDTO.java
@@ -0,0 +1,24 @@
+/*********************************************************************
+* Copyright (c) 2022 Contributors to the Eclipse Foundation.
+*
+* This program and the accompanying materials are made
+* available under the terms of the Eclipse Public License 2.0
+* which is available at https://www.eclipse.org/legal/epl-2.0/
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Contributors:
+*   Kentyou - initial implementation
+**********************************************************************/
+package org.eclipse.sensinact.gateway.southbound.http.factory.config;
+
+/**
+ * Root configuration of a device factory
+ */
+public class HttpDeviceFactoryConfigurationPeriodicDTO extends HttpDeviceFactoryConfigurationTaskDTO {
+
+    /**
+     * Refresh period in seconds (5 minutes by default)
+     */
+    public int period = 300;
+}
