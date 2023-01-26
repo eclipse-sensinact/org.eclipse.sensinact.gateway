dad4092fd2505dc6e8e28a9efe563be3ac8162ff
Tim Ward <timothyjward@apache.org>
2023-01-26 14:21:09 +0100

    Update prototype/core/impl/src/main/java/org/eclipse/sensinact/prototype/model/nexus/impl/emf/EMFUtil.java
    Co-authored-by: Jürgen Albert <j.albert@data-in-motion.biz>

--- prototype/core/impl/src/main/java/org/eclipse/sensinact/prototype/model/nexus/impl/emf/EMFUtil.java
+++ prototype/core/impl/src/main/java/org/eclipse/sensinact/prototype/model/nexus/impl/emf/EMFUtil.java
@@ -17,7 +17,6 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.function.Function;
 
 import org.eclipse.emf.common.util.URI;
