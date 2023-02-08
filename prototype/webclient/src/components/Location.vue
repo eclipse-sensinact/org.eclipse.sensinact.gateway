36745a4bc95530221956ccfb98d84d14672ecf7f
Markus Hochstein <markus.hochstein@sdace.de>
2023-02-08 12:01:53 +0100

    add licences headers

--- prototype/webclient/src/components/Location.vue
+++ prototype/webclient/src/components/Location.vue
@@ -1,3 +1,17 @@
+<!--
+  Copyright (c) YYYY Contributors to the  Eclipse Foundation.
+
+  This program and the accompanying materials are made
+  available under the terms of the Eclipse Public License 2.0
+  which is available at https://www.eclipse.org/legal/epl-2.0/
+
+  SPDX-License-Identifier: EPL-2.0
+
+  Contributors:
+     Markus Hochstein
+-->
+
+
 <template>
   <div class="t1">
       <b-loading :active="loading" :can-cancel="false" :is-full-page="false"></b-loading>
