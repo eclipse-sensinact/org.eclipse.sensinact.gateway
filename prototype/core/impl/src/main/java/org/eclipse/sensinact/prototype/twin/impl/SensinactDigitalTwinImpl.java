ac4d460d91b1d9dcfafa42334aa28934f409db66
Tim Ward <timothyjward@apache.org>
2023-03-15 19:08:18 +0000

    Bugs in the Sensorthings API
    * Use the correct provider to create Observation ids
    * Avoid NPE when filtering providers with null services
    
    Signed-off-by: Tim Ward <timothyjward@apache.org>

--- prototype/core/impl/src/main/java/org/eclipse/sensinact/prototype/twin/impl/SensinactDigitalTwinImpl.java
+++ prototype/core/impl/src/main/java/org/eclipse/sensinact/prototype/twin/impl/SensinactDigitalTwinImpl.java
@@ -264,7 +264,7 @@ public class SensinactDigitalTwinImpl extends CommandScopedImpl implements Sensi
         // Filter providers according to their services
         providersStream = providersStream.map(p -> {
             final Provider modelProvider = p.getModelProvider();
-            modelProvider.eClass().getEStructuralFeatures().stream().forEach((feature) -> {
+            nexusImpl.getServicesForModel(modelProvider.eClass()).forEach((feature) -> {
                 p.add(new ServiceSnapshotImpl(p, feature.getName(), (Service) modelProvider.eGet(feature),
                         snapshotTime));
             });
@@ -277,7 +277,9 @@ public class SensinactDigitalTwinImpl extends CommandScopedImpl implements Sensi
         // Filter providers according to their resources
         providersStream = providersStream.map(p -> {
             p.getServices().stream().forEach(s -> {
-                s.getModelService().eClass().getEStructuralFeatures().stream()
+                // Avoid ModelService which may be null
+                EStructuralFeature sf = s.getProvider().getModelProvider().eClass().getEStructuralFeature(s.getName());
+                nexusImpl.getResourcesForService((EClass) sf.getEType())
                         .forEach(f -> s.add(new ResourceSnapshotImpl(s, f, snapshotTime)));
             });
             return p;
@@ -295,7 +297,7 @@ public class SensinactDigitalTwinImpl extends CommandScopedImpl implements Sensi
                     final Service svc = rc.getService().getModelService();
                     final ETypedElement rcFeature = rc.getFeature();
 
-                    final ResourceMetadata metadata = svc.getMetadata().get(rcFeature);
+                    final ResourceMetadata metadata = svc == null ? null : svc.getMetadata().get(rcFeature);
                     final Instant timestamp;
                     if (metadata != null) {
                         timestamp = metadata.getTimestamp();
@@ -303,7 +305,8 @@ public class SensinactDigitalTwinImpl extends CommandScopedImpl implements Sensi
                         timestamp = null;
                     }
 
-                    rc.setValue(new TimedValueImpl<Object>(svc.eGet((EStructuralFeature) rcFeature), timestamp));
+                    rc.setValue(new TimedValueImpl<Object>(
+                            svc == null ? null : svc.eGet((EStructuralFeature) rcFeature), timestamp));
                 });
             });
             p.filterEmptyServices();
