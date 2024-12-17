b0c739529afb032516f9903c002ff1ea1a187a2e
Thomas Calmant <thomas.calmant@gmail.com>
2024-12-17 15:02:42 +0100

    Added a simple Java URL-based ThingDescription loader

---
+++ southbound/wot/core/src/main/java/org/eclipse/sensinact/gateway/southbound/wot/core/loader/LoaderConfiguration.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright 2024 Kentyou
+ * Proprietary and confidential
+ *
+ * All Rights Reserved.
+ * Unauthorized copying of this file is strictly prohibited
+ */
+package org.eclipse.sensinact.gateway.southbound.wot.core.loader;
+
+public @interface LoaderConfiguration {
+
+    /**
+     * Target URI
+     */
+    String uri();
+}
