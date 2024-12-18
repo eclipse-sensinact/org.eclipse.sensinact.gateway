266e2672dac748a9105d1ca0b1a4b676a282ab2b
Thomas Calmant <thomas.calmant@gmail.com>
2024-12-18 15:24:34 +0100

    Fixed license header

--- southbound/wot/core/src/main/java/org/eclipse/sensinact/gateway/southbound/wot/core/loader/LoaderConfiguration.java
+++ southbound/wot/core/src/main/java/org/eclipse/sensinact/gateway/southbound/wot/core/loader/LoaderConfiguration.java
@@ -1,10 +1,16 @@
-/*
- * Copyright 2024 Kentyou
- * Proprietary and confidential
- *
- * All Rights Reserved.
- * Unauthorized copying of this file is strictly prohibited
- */
+/*********************************************************************
+* Copyright (c) 2024 Contributors to the Eclipse Foundation.
+*
+* This program and the accompanying materials are made
+* available under the terms of the Eclipse Public License 2.0
+* which is available at https://www.eclipse.org/legal/epl-2.0/
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Contributors:
+*   Kentyou - initial implementation
+**********************************************************************/
+
 package org.eclipse.sensinact.gateway.southbound.wot.core.loader;
 
 public @interface LoaderConfiguration {
