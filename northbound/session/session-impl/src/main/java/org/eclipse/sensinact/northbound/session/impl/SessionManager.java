de2d6c5122c51445cbb4fdad252c468cb6672382
Thomas Calmant <thomas.calmant@gmail.com>
2025-06-03 15:44:39 +0200

    Fixed typo in package name
    Signed-off-by: Thomas Calmant <thomas.calmant@gmail.com>

--- northbound/session/session-impl/src/main/java/org/eclipse/sensinact/northbound/session/impl/SessionManager.java
+++ northbound/session/session-impl/src/main/java/org/eclipse/sensinact/northbound/session/impl/SessionManager.java
@@ -10,7 +10,7 @@
 * Contributors:
 *   Kentyou - initial implementation
 **********************************************************************/
-package org.eclipse.sensinact.nortbound.session.impl;
+package org.eclipse.sensinact.northbound.session.impl;
 
 import static java.util.stream.Collectors.toCollection;
 import static java.util.stream.Collectors.toList;
