ca787987c490f84e53d5ae3e1413c2567021ae78
Thomas Calmant <thomas.calmant@gmail.com>
2025-07-01 10:21:19 +0200

    Fixed wrong copyright header

--- northbound/security/authorization-casbin/src/test/java/org/eclipse/sensinact/northbound/security/authorization/casbin/integration/FineAuthorizationTest.java
+++ northbound/security/authorization-casbin/src/test/java/org/eclipse/sensinact/northbound/security/authorization/casbin/integration/FineAuthorizationTest.java
@@ -1,10 +1,14 @@
-/*
- * Copyright 2025 Kentyou
- * Proprietary and confidential
+/*********************************************************************
+ * Copyright (c) 2025 Contributors to the Eclipse Foundation.
  *
- * All Rights Reserved.
- * Unauthorized copying of this file is strictly prohibited
- */
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors: Kentyou - initial implementation
+ **********************************************************************/
 
 package org.eclipse.sensinact.northbound.security.authorization.casbin.integration;
 
