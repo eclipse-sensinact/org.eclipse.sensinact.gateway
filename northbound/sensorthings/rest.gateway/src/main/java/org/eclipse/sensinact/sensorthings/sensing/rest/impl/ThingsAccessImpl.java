c52e05f9c0e8534c07f119c45147632de7149248
pisua <aurelien.pisu@kentyou.com>
2025-12-01 15:39:01 +0100

    add Update interface

--- northbound/sensorthings/rest.gateway/src/main/java/org/eclipse/sensinact/sensorthings/sensing/rest/impl/ThingsAccessImpl.java
+++ northbound/sensorthings/rest.gateway/src/main/java/org/eclipse/sensinact/sensorthings/sensing/rest/impl/ThingsAccessImpl.java
@@ -37,14 +37,16 @@ import org.eclipse.sensinact.sensorthings.sensing.dto.Sensor;
 import org.eclipse.sensinact.sensorthings.sensing.dto.Thing;
 import org.eclipse.sensinact.sensorthings.sensing.dto.expand.ExpandedDataStream;
 import org.eclipse.sensinact.sensorthings.sensing.dto.expand.ExpandedLocation;
+import org.eclipse.sensinact.sensorthings.sensing.dto.expand.ExpandedThing;
 import org.eclipse.sensinact.sensorthings.sensing.rest.access.ThingsAccess;
 import org.eclipse.sensinact.sensorthings.sensing.rest.annotation.PaginationLimit;
 import org.eclipse.sensinact.sensorthings.sensing.rest.create.ThingsCreate;
+import org.eclipse.sensinact.sensorthings.sensing.rest.update.ThingsUpdate;
 
 import jakarta.ws.rs.NotFoundException;
 import jakarta.ws.rs.core.Response;
 
-public class ThingsAccessImpl extends AbstractAccess implements ThingsAccess, ThingsCreate {
+public class ThingsAccessImpl extends AbstractAccess implements ThingsAccess, ThingsCreate, ThingsUpdate {
 
     @Override
     public Thing getThing(String id) {
@@ -277,5 +279,23 @@ public class ThingsAccessImpl extends AbstractAccess implements ThingsAccess, Th
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("not yet implemented");
 
+    }
+
+    @Override
+    public Response updateDatastream(String id, String id2, ExpandedDataStream datastream) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Response updateLocation(String id, String id2, ExpandedLocation location) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Response updateThing(String id, ExpandedThing thing) {
+        // TODO Auto-generated method stub
+        return null;
     }
 }
