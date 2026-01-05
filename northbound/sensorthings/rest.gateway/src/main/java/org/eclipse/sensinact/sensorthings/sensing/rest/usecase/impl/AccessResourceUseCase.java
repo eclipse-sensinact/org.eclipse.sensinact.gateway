e79062f51dd6e3ae94727232378ac50fa31701ed
Aurélien Pisu <aurelien.pisu@kentyou.com>
2026-01-05 15:41:58 +0100

    refactoring and add generics for useCase snapshot in response

--- northbound/sensorthings/rest.gateway/src/main/java/org/eclipse/sensinact/sensorthings/sensing/rest/usecase/impl/AccessResourceUseCase.java
+++ northbound/sensorthings/rest.gateway/src/main/java/org/eclipse/sensinact/sensorthings/sensing/rest/usecase/impl/AccessResourceUseCase.java
@@ -3,9 +3,9 @@ package org.eclipse.sensinact.sensorthings.sensing.rest.usecase.impl;
 import org.eclipse.sensinact.core.snapshot.ProviderSnapshot;
 import org.eclipse.sensinact.core.snapshot.ResourceSnapshot;
 import org.eclipse.sensinact.northbound.session.SensiNactSession;
-import org.eclipse.sensinact.sensorthings.sensing.dto.expand.Helpers;
 import org.eclipse.sensinact.sensorthings.sensing.rest.access.IAccessProviderUseCase;
 import org.eclipse.sensinact.sensorthings.sensing.rest.access.IAccessResourceUseCase;
+import org.eclipse.sensinact.sensorthings.sensing.rest.impl.DtoMapper;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ReferenceCardinality;
@@ -18,12 +18,12 @@ public class AccessResourceUseCase implements IAccessResourceUseCase {
 
     @Override
     public ResourceSnapshot read(SensiNactSession session, String id) {
-        String providerId = Helpers.extractFirstIdSegment(id);
+        String providerId = DtoMapper.extractFirstIdSegment(id);
 
         ProviderSnapshot providerSnapshot = validateAndGetProvider(session, providerId);
 
-        String service = Helpers.extractFirstIdSegment(id.substring(providerId.length() + 1));
-        String resource = Helpers.extractFirstIdSegment(id.substring(providerId.length() + service.length() + 2));
+        String service = DtoMapper.extractFirstIdSegment(id.substring(providerId.length() + 1));
+        String resource = DtoMapper.extractFirstIdSegment(id.substring(providerId.length() + service.length() + 2));
 
         ResourceSnapshot resourceSnapshot = providerSnapshot.getResource(service, resource);
 
