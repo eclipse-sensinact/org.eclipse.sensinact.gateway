01ca096a6455f61e69bf4702c41dcb091113a556
Aurélien Pisu <aurelien.pisu@kentyou.com>
2026-01-05 15:41:59 +0100

    fix model

--- northbound/sensorthings/rest.gateway.extra/src/main/java/org/eclipse/sensinact/sensorthings/sensing/dto/expand/update/LocationUpdate.java
+++ northbound/sensorthings/rest.gateway.extra/src/main/java/org/eclipse/sensinact/sensorthings/sensing/dto/expand/update/LocationUpdate.java
@@ -20,17 +20,15 @@ import org.eclipse.sensinact.core.annotation.dto.Data;
 import org.eclipse.sensinact.core.annotation.dto.Model;
 import org.eclipse.sensinact.core.annotation.dto.Provider;
 import org.eclipse.sensinact.core.annotation.dto.Service;
-import org.eclipse.sensinact.core.annotation.dto.ServiceModel;
 import org.eclipse.sensinact.gateway.geojson.GeoJsonObject;
 import org.eclipse.sensinact.sensorthings.sensing.dto.expand.SensorThingsUpdate;
 import static org.eclipse.sensinact.sensorthings.models.extended.ExtendedPackage.Literals.SENSOR_THING_LOCATION;
-import static org.eclipse.sensinact.sensorthings.models.extended.ExtendedPackage.Literals.SENSOR_THING_LOCATION_SERVICE;
 
 /**
  * Update record for the Location of a SensorThing / Device
  */
 @Service("admin")
-public record LocationUpdate(@Model EClass model, @ServiceModel EClass service, @Provider String providerId,
+public record LocationUpdate(@Model EClass model, @Provider String providerId,
         @Service("location") @Data(onDuplicate = UPDATE_IF_DIFFERENT) Object id,
         @Service("location") @Data(onDuplicate = UPDATE_IF_DIFFERENT, onNull = UPDATE_IF_PRESENT) String name,
         @Service("location") @Data(onDuplicate = UPDATE_IF_DIFFERENT, onNull = UPDATE_IF_PRESENT) String description,
@@ -45,18 +43,11 @@ public record LocationUpdate(@Model EClass model, @ServiceModel EClass service, 
         if (model != SENSOR_THING_LOCATION) {
             throw new IllegalArgumentException("The model for the provider must be " + SENSOR_THING_LOCATION.getName());
         }
-        if (service == null) {
-            service = SENSOR_THING_LOCATION_SERVICE;
-        }
-        if (service != SENSOR_THING_LOCATION_SERVICE) {
-            throw new IllegalArgumentException(
-                    "The model for the thing must be " + SENSOR_THING_LOCATION_SERVICE.getName());
-        }
+
     }
 
     public LocationUpdate(String providerId, Object id, String name, String description, String encodingType,
             GeoJsonObject location) {
-        this(SENSOR_THING_LOCATION, SENSOR_THING_LOCATION_SERVICE, providerId, id, name, description, encodingType,
-                location);
+        this(SENSOR_THING_LOCATION, providerId, id, name, description, encodingType, location);
     }
 }
