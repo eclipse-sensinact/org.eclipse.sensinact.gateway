8b6d7679db59347382bf3c6559184a847888b14e
Aurélien Pisu <aurelien.pisu@kentyou.com>
2026-01-05 15:41:57 +0100

    start implementation, refactoring

---
+++ northbound/sensorthings/rest.gateway.extra/src/test/java/org/eclipse/sensinact/sensorthings/sensing/rest/extra/impl/integration/CreateTest.java
@@ -0,0 +1,38 @@
+package org.eclipse.sensinact.sensorthings.sensing.rest.extra.impl.integration;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class CreateTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public CreateTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( CreateTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
