4a459edb80f16e6b48bc66c41f1252fd5b90e075
Aurélien Pisu <aurelien.pisu@kentyou.com>
2026-01-05 15:41:58 +0100

    add commentary

--- northbound/sensorthings/rest.gateway.extra/src/main/java/org/eclipse/sensinact/sensorthings/sensing/rest/extra/usecase/IObservedPropertyExtraUseCase.java
+++ northbound/sensorthings/rest.gateway.extra/src/main/java/org/eclipse/sensinact/sensorthings/sensing/rest/extra/usecase/IObservedPropertyExtraUseCase.java
@@ -1,10 +1,34 @@
+/*********************************************************************
+* Copyright (c) 2025 Contributors to the Eclipse Foundation.
+*
+* This program and the accompanying materials are made
+* available under the terms of the Eclipse Public License 2.0
+* which is available at https://www.eclipse.org/legal/epl-2.0/
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Contributors:
+*   Kentyou - initial implementation
+**********************************************************************/
 package org.eclipse.sensinact.sensorthings.sensing.rest.extra.usecase;
 
 import org.eclipse.sensinact.sensorthings.sensing.dto.expand.ExpandedObservedProperty;
 
 public interface IObservedPropertyExtraUseCase {
+    /**
+     * return cache observed property base on id else null
+     *
+     * @param id
+     * @return
+     */
     public ExpandedObservedProperty getInMemoryObservedProperty(String id);
 
+    /**
+     * remove cached observed property base oln id if exists
+     *
+     * @param id
+     * @return
+     */
     public ExpandedObservedProperty removeInMemoryObservedProperty(String id);
 
 }
--- northbound/sensorthings/rest.gateway.extra/src/main/java/org/eclipse/sensinact/sensorthings/sensing/rest/extra/usecase/ISensorExtraUseCase.java
+++ northbound/sensorthings/rest.gateway.extra/src/main/java/org/eclipse/sensinact/sensorthings/sensing/rest/extra/usecase/ISensorExtraUseCase.java
@@ -1,10 +1,34 @@
+/*********************************************************************
+* Copyright (c) 2025 Contributors to the Eclipse Foundation.
+*
+* This program and the accompanying materials are made
+* available under the terms of the Eclipse Public License 2.0
+* which is available at https://www.eclipse.org/legal/epl-2.0/
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Contributors:
+*   Kentyou - initial implementation
+**********************************************************************/
 package org.eclipse.sensinact.sensorthings.sensing.rest.extra.usecase;
 
 import org.eclipse.sensinact.sensorthings.sensing.dto.expand.ExpandedSensor;
 
 public interface ISensorExtraUseCase {
+    /**
+     * return cached sensor or null based on id if exists
+     *
+     * @param id
+     * @return
+     */
     public ExpandedSensor getInMemorySensor(String id);
 
+    /**
+     * remove cache sensor based on id if exists
+     *
+     * @param id
+     * @return
+     */
     public ExpandedSensor removeInMemorySensor(String id);
 
 }
