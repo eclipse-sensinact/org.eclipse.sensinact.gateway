fc33a7aa96e0c2b53b49bee844ea099b74061878
Aurélien Pisu <aurelien.pisu@kentyou.com>
2026-01-05 15:41:57 +0100

    add update, create endpoint

---
+++ northbound/sensorthings/rest.api/src/main/java/org/eclipse/sensinact/sensorthings/sensing/rest/update/HistoricalLocationsUpdate.java
@@ -0,0 +1,27 @@
+/*********************************************************************
+* Copyright (c) 2022 Contributors to the Eclipse Foundation.
+*
+* This program and the accompanying materials are made
+* available under the terms of the Eclipse Public License 2.0
+* which is available at https://www.eclipse.org/legal/epl-2.0/
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Contributors:
+*   Kentyou - initial implementation
+**********************************************************************/
+package org.eclipse.sensinact.sensorthings.sensing.rest.update;
+
+import org.eclipse.sensinact.sensorthings.sensing.dto.HistoricalLocation;
+
+import jakarta.ws.rs.PATCH;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.core.Response;
+
+public interface HistoricalLocationsUpdate {
+
+    @PUT
+    @PATCH
+    public Response updateHistoricalLocation(@PathParam("id") String id, HistoricalLocation location);
+}
