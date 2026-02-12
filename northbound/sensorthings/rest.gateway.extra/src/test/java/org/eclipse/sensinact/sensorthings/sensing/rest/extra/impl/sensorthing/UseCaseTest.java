181db87c37e8b0aa94ae53049e522019112e6e99
Aurélien Pisu <aurelien.pisu@kentyou.com>
2026-02-12 15:34:06 +0100

    fix log + manage exception loggin

--- northbound/sensorthings/rest.gateway.extra/src/test/java/org/eclipse/sensinact/sensorthings/sensing/rest/extra/impl/sensorthing/UseCaseTest.java
+++ northbound/sensorthings/rest.gateway.extra/src/test/java/org/eclipse/sensinact/sensorthings/sensing/rest/extra/impl/sensorthing/UseCaseTest.java
@@ -1,16 +1,16 @@
 /*********************************************************************
- * Copyright (c) 2026 Contributors to the Eclipse Foundation.
- *
- * This program and the accompanying materials are made
- * available under the terms of the Eclipse Public License 2.0
- * which is available at https://www.eclipse.org/legal/epl-2.0/
- *
- * SPDX-License-Identifier: EPL-2.0
- *
- * Contributors:
- *   Kentyou - initial implementation
- **********************************************************************/
-package org.eclipse.sensinact.sensorthings.sensing.rest.extra.impl.sensorthing;
+* Copyright (c) 2026 Contributors to the Eclipse Foundation.
+*
+* This program and the accompanying materials are made
+* available under the terms of the Eclipse Public License 2.0
+* which is available at https://www.eclipse.org/legal/epl-2.0/
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Contributors:
+*   Kentyou - initial implementation
+**********************************************************************/
+package org.eclipse.sensinact.sensorthings.sensing.rest.extra.endpoint;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mock.Strictness.LENIENT;
@@ -18,9 +18,10 @@ import static org.mockito.Mock.Strictness.LENIENT;
 import org.eclipse.sensinact.core.command.GatewayThread;
 import org.eclipse.sensinact.core.push.DataUpdate;
 import org.eclipse.sensinact.sensorthings.sensing.rest.access.IAccessProviderUseCase;
+import org.eclipse.sensinact.sensorthings.sensing.rest.access.IAccessResourceUseCase;
 import org.eclipse.sensinact.sensorthings.sensing.rest.access.IAccessServiceUseCase;
+import org.eclipse.sensinact.sensorthings.sensing.rest.usecase.impl.DtoMemoryCacheProvider;
 import org.eclipse.sensinact.sensorthings.sensing.rest.access.IDtoMemoryCache;
-import org.eclipse.sensinact.sensorthings.sensing.rest.extra.endpoint.UseCaseProvider;
 import org.eclipse.sensinact.sensorthings.sensing.rest.extra.usecase.DatastreamsExtraUseCase;
 import org.eclipse.sensinact.sensorthings.sensing.rest.extra.usecase.FeatureOfInterestExtraUseCase;
 import org.eclipse.sensinact.sensorthings.sensing.rest.extra.usecase.IExtraUseCase;
@@ -31,6 +32,7 @@ import org.eclipse.sensinact.sensorthings.sensing.rest.extra.usecase.RefIdUseCas
 import org.eclipse.sensinact.sensorthings.sensing.rest.extra.usecase.SensorsExtraUseCase;
 import org.eclipse.sensinact.sensorthings.sensing.rest.extra.usecase.ThingsExtraUseCase;
 import org.eclipse.sensinact.sensorthings.sensing.rest.usecase.impl.AccessProviderUseCaseProvider;
+import org.eclipse.sensinact.sensorthings.sensing.rest.usecase.impl.AccessResourceUseCaseProvider;
 import org.eclipse.sensinact.sensorthings.sensing.rest.usecase.impl.AccessServiceUseCaseProvider;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -39,9 +41,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.eclipse.sensinact.sensorthings.sensing.rest.extra.endpoint.GatewayThreadProvider;
-import org.eclipse.sensinact.sensorthings.sensing.rest.extra.endpoint.DataUpdateProvider;
-import org.eclipse.sensinact.sensorthings.sensing.rest.usecase.impl.DtoMemoryCacheProvider;
+
 import jakarta.ws.rs.ext.Providers;
 
 @ExtendWith(MockitoExtension.class)
@@ -78,6 +78,9 @@ class UseCaseTest {
         Mockito.when(providers.<IAccessServiceUseCase>getContextResolver(
                 Mockito.argThat(c -> c != null && IAccessServiceUseCase.class.isAssignableFrom(c)), Mockito.any()))
                 .thenReturn(new AccessServiceUseCaseProvider());
+        Mockito.when(providers.<IAccessResourceUseCase>getContextResolver(
+                Mockito.argThat(c -> c != null && IAccessResourceUseCase.class.isAssignableFrom(c)), Mockito.any()))
+                .thenReturn(new AccessResourceUseCaseProvider());
         Mockito.when(providers.<GatewayThread>getContextResolver(
                 Mockito.argThat(c -> c != null && GatewayThread.class.isAssignableFrom(c)), Mockito.any()))
                 .thenReturn(new GatewayThreadProvider(gatewayThread));
