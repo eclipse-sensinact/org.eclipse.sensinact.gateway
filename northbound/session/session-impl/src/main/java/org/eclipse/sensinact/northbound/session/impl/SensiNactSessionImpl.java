9f86a6a6d1b9782d233b0a7d4b69e37514ebbd7d
Thomas Calmant <thomas.calmant@gmail.com>
2026-04-20 17:01:11 +0200

    Handle special case of multi-values when getting resource content
    Signed-off-by: Thomas Calmant <thomas.calmant@gmail.com>

--- northbound/session/session-impl/src/main/java/org/eclipse/sensinact/northbound/session/impl/SensiNactSessionImpl.java
+++ northbound/session/session-impl/src/main/java/org/eclipse/sensinact/northbound/session/impl/SensiNactSessionImpl.java
@@ -475,13 +475,23 @@ public class SensiNactSessionImpl implements SensiNactSession {
                             break;
 
                         default:
-                            val = sensinactResource.getValue(Object.class, GetLevel.NORMAL).map(tv -> {
-                                // Add the current value
-                                result.valueType = ValueType.UPDATABLE;
-                                result.value = tv.getValue();
-                                result.timestamp = tv.getTimestamp();
-                                return result;
-                            });
+                            if (sensinactResource.isMultiple()) {
+                                val = sensinactResource.getMultiValue(Object.class, GetLevel.NORMAL).map(tv -> {
+                                    // Add the current values
+                                    result.valueType = ValueType.UPDATABLE;
+                                    result.value = tv.getValue();
+                                    result.timestamp = tv.getTimestamp();
+                                    return result;
+                                });
+                            } else {
+                                val = sensinactResource.getValue(Object.class, GetLevel.NORMAL).map(tv -> {
+                                    // Add the current value
+                                    result.valueType = ValueType.UPDATABLE;
+                                    result.value = tv.getValue();
+                                    result.timestamp = tv.getTimestamp();
+                                    return result;
+                                });
+                            }
                             break;
                         }
 
